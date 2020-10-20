1b994e01871ac2996f5814b70b988c6e04fed730
Hugo Lima Romão <hugo8romao@gmail.com>
2020-10-20 19:20:19 -0300

    Deixando o nome do site dinâmico

--- wp-content/themes/Mugeo/header.php
+++ wp-content/themes/Mugeo/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mugeo - Museu geológico UFRR</title>
+  <title><?php bloginfo('name'); ?></title>
   <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;700&family=Cardo:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/responsivo.css">
